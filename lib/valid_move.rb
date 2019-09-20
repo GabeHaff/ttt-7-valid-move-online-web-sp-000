88805f43b872ada74c4262a7b5c3c1754bb2174c
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-20 13:23:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,12 @@
 # code your #valid_move? method here
 
 def valid_move?
-  if position_taken? 
+  input=gets.chomp 
+  if input== 0-8
+    return true 
+  elsif position_taken? 
     return false 
+    
 end 
 end 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
